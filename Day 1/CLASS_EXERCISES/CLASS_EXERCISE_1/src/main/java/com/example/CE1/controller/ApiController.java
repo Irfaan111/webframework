b42581d96df08535c3c4a85231f5577479808d55
Irfaan111 <150134672+Irfaan111@users.noreply.github.com>
2024-04-13 12:56:36 +0530

    Add files via upload

---
+++ Day 1/CLASS_EXERCISES/CLASS_EXERCISE_1/src/main/java/com/example/CE1/controller/ApiController.java
@@ -0,0 +1,13 @@
+package com.example.CE1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+    @GetMapping("/welcome")
+    public String getMethodName()
+    {
+        return "Welcome Spring Boot!";
+    }
+}
